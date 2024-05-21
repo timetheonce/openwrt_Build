dde65966e3f29371464716539fbe88d0a785eb81
timetheonce <49587703+timetheonce@users.noreply.github.com>
2024-05-21 16:18:01 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #===============================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.251/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/kenzo/g' package/base-files/files/bin/config_generate
